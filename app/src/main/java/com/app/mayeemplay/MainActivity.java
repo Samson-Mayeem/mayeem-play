44be0e9599ebea4c5513db1c1752350c9dcd14e5
Samson-Mayeem <samsonmayeem@gmail.com>
2022-09-20 00:29:42 +0000

    deploy to fix failure in CircleCI

--- app/src/main/java/com/app/mayeemplay/MainActivity.java
+++ app/src/main/java/com/app/mayeemplay/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -67,9 +71,38 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<File> mySongs = findsong(Environment.getExternalStorageDirectory());
         items = new String[mySongs.size()];
         for (int i = 0; i < mySongs.size(); i++){
-            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "");
+            items[i] = mySongs.get(i).getName().toString().replace(".mp3", "").replace(".wav", "");
         }
-        ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
-        listView.setAdapter(myAdapter);
+        /*ArrayAdapter<String> myAdapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(myAdapter);*/
+        customAdapter customAdapter = new customAdapter();
+        listView.setAdapter(customAdapter);
     }
+    class customAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View myview = getLayoutInflater().inflate(R.layout.list_item, null);
+            TextView textsong = myview.findViewById(R.id.txtsongname);
+            textsong.setSelected(true);
+            textsong.setText(items[i]);
+            return myview;
+        }
+    }
+
 }
